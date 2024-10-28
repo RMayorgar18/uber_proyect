aec7debb68fe1100e142739c8936edcb47a6ffc7
Nicolas. Z <ni.zunigao@duocuc.cl>
2024-10-27 23:37:22 -0300

    vista login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserModel } from 'src/app/models/usuario';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { HelperService } from 'src/app/services/helper.service';
 import { StorageService } from 'src/app/services/storage.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
 
 @Component({
   selector: 'app-login',
@@ -11,18 +14,20 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class LoginPage implements OnInit {
 
-  correo:string = "";
+  correo:string ="";
   contrasena:string = "";
-
-
+  token:string = "";
+  usuario:UserModel[] = [];
 
 
 
   constructor(private router:Router, 
-    private firebase:FirebaseService, 
-    private helper:HelperService,
-    private storage: StorageService
-  ) { }
+              private firebase:FirebaseService, 
+              private helper:HelperService,
+              private storage:StorageService,
+              private usuarioService:UsuarioService
+            
+            ) { }
 
   ngOnInit() {
   }
@@ -44,59 +49,71 @@ export class LoginPage implements OnInit {
       alert("Credenciales incorrectas.");
     } */
 
-    const loader = await this.helper.showLoader("Cargando");
-    //solicitud get use
-
-    try {
-
-      await this.firebase.login(this.correo,this.contrasena);
-      //solicitur get user
-      
-      loader.dismiss();
-    } catch (error:any) {
-      
-      let msg = "Ocurrió un error al iniciar sesión.";
-      
-      if(error.code == "auth/invalid-credential1"){
-        msg = "Credenciales incorrectas.";
-      }else if(error.code == "auth/wrong-password1"){
-        msg = "Contraseña incorrecta.";
-      }else if(error.code == "auth/invalid-email1"){
-        msg = "Correo no valido.";
+      const loader = await this.helper.showLoader("Cargando");
+      try {
+  
+        const reqFirebase = await this.firebase.login(this.correo,this.contrasena);
+        //solicitud get user
+        const token = await reqFirebase.user?.getIdToken();
+  
+        if (token) {
+          this.token = token;
+          const req = await this.usuarioService.obtenerUsuario(
+            {
+              p_correo:this.correo,
+              token:token
+            }
+          );
+          this.usuario = req.data;
+          console.log("DATA USUARIO", this.usuario[0].id_usuario);
+          
+        }
+        
+        loader.dismiss();
+      } catch (error:any) {
+        
+        let msg = "error al iniciar sesión.";
+        
+        if(error.code == "auth/invalid-credential1"){
+          msg = "Credenciales incorrectas.";
+        }else if(error.code == "auth/wrong-password1"){
+          msg = "Contraseña incorrecta.";
+        }else if(error.code == "auth/invalid-email1"){
+          msg = "Correo no valido.";
+        }
+  
+  
+        this.helper.showAlert(msg,"Aceptar");
+        loader.dismiss();
       }
-
-
-      this.helper.showAlert(msg,"Aceptar");
-      loader.dismiss();
+  
+      const jsonToken = 
+      [
+        {
+          "token":this.token,
+          "usuario_id":this.usuario[0].id_usuario,
+          "usuario_correo":this.usuario[0].correo_electronico
+        }
+      ];
+  
+      this.storage.agregarToken(jsonToken);
+  
+  
+  
+      //Obtenemos la info que guardamos en storage
+      let token = await this.storage.obtenerStorage();
+      console.log(token[0].usuario_correo);
+      
+      await this.helper.showToast("Login correcto!!!!!");
+      this.router.navigateByUrl("/inicio");
     }
-
-    const jsonToken = 
-    [
-      {
-        "token":"123hbkjasnbdkjbsdkjs123",
-        "nombre":"PGY4121",
-        "usuario_correo":""
-      }
-    ];
-
-    this.storage.agregarToken(jsonToken);
-
-
-
-    //Obtenemos la info que guardamos en storage
-    let token = await this.storage.obtenerStorage();
-    console.log(token[0].nombre);
-    
-
-    this.router.navigateByUrl("/inicio");
-  }
-
-  resetPw(){
-    this.router.navigateByUrl("reset-password");
-  }
-
-  registro(){
-    this.router.navigateByUrl("registro");
-  }
+  
+    resetPw(){
+      this.router.navigateByUrl("reset-password");
+    }
+  
+    registro(){
+      this.router.navigateByUrl("registro");
+    }
 
 }
